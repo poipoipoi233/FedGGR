a94c85b740213c1823bf7c24fd02dd633430924c
poipoipoi233 <2214792801@qq.com>
2023-01-19 15:22:41 +0800

    Delete redundant files

--- federatedscope/FedGSL/run_fedgsl.sh
+++ federatedscope/FedGSL/run_fedgsl.sh
@@ -2,143 +2,73 @@ set -e
 
 cd ../../
 
-cudaid=0
-dataset=pubmed
+cudaid=$1
+dataset=$2
 method=fedgsl
-splitter=random
+splitter=random #or louvain
 client_num=(3 5 10)
-backbone='gat' #gcn gat gpr
-echo "run starts..."
+backbone='gcn'
 
-batch_size=1 #来自zhl的提醒:用sage方法的时候需要在if里面指定特殊的batch_size
-generator='MLP-D' #指定GSL图生成器类型
-patience=30
+patience=200
 seed=(0 1 2 3 4)
-if [[ $backbone = 'gcn' ]];then
-  model_type='gnn_fedgsl_gcn'
-  if [[ $dataset = 'cora' ]]; then
-    out_channels=7
-    op=Adam
-    hidden=256
-    dropout=0.5
-    local_updates=1
-    weight_decay=0.
-    gsl_gnn_hids=256
-    loc_gnn_outsize=256
-    glob_gnn_outsize=256
-    pretrain_out_channels=64
-    k_for_knn=15
-    lr=0.01
-  elif [[ $dataset = 'citeseer' ]]; then
-    out_channels=6
-    op=Adam
-    hidden=64
-    dropout=0.5
-    local_updates=2
-    weight_decay=0.
-    gsl_gnn_hids=64
-    loc_gnn_outsize=64
-    glob_gnn_outsize=64
-    pretrain_out_channels=64
-    k_for_knn=15
-    lr=0.001
-  elif [[ $dataset = 'pubmed' ]]; then
-    out_channels=3
-    op=Adam
-    hidden=64
-    dropout=0.
-    local_updates=1
-    weight_decay=0.
-    gsl_gnn_hids=256
-    loc_gnn_outsize=64
-    glob_gnn_outsize=256
-    pretrain_out_channels=256
-    k_for_knn=30
-    lr=0.03
-    server_lr=0.03 #note 其他dataset里每设置这个
-  elif [[ $dataset = 'computers' ]]; then
-    out_channels=10
-    op=Adam
-    hidden=64
-    dropout=0.5
-    local_updates=3
-    weight_decay=0.0
-    gsl_gnn_hids=64
-    loc_gnn_outsize=256
-    glob_gnn_outsize=64
-    pretrain_out_channels=256
-    k_for_knn=20
-    lr=0.01
-  fi
-elif [[ $backbone = 'gat' ]]; then
-  model_type='gnn_fedgsl_gat'
-  if [[ $dataset = 'citeseer' ]]; then
-    out_channels=6
-    op=Adam
-    hidden=256
-    dropout=0.5
-    local_updates=3
-    weight_decay=0.
-    gsl_gnn_hids=64
-    loc_gnn_outsize=64
-    glob_gnn_outsize=64
-    pretrain_out_channels=64
-    k_for_knn=15
-    lr=0.001
-  elif [[ $dataset = 'pubmed' ]]; then
-    out_channels=3
-    op=Adam
-    hidden=64
-    dropout=0.
-    local_updates=1
-    weight_decay=0.
-    gsl_gnn_hids=256
-    loc_gnn_outsize=64
-    glob_gnn_outsize=256
-    pretrain_out_channels=256
-    k_for_knn=30
-    lr=0.03
-    server_lr=0.03 #note 其他dataset里每设置这个
-  fi
-elif [[ $backbone = 'gpr' ]]; then
-  model_type='gnn_fedgsl_gpr'
-  if [[ $dataset = 'citeseer' ]]; then
-    out_channels=6
-    op=Adam
-    hidden=64
-    dropout=0.5
-    local_updates=2
-    weight_decay=0.
-    gsl_gnn_hids=64
-    loc_gnn_outsize=64
-    glob_gnn_outsize=64
-    pretrain_out_channels=64
-    k_for_knn=0.5
-    lr=0.001
-    server_lr=0.001
-  fi
-elif [[ $backbone = 'gin' ]]; then
-  model_type='gnn_fedgsl_gin'
-  if [[ $dataset = 'citeseer' ]]; then
-    out_channels=6
-    op=Adam
-    hidden=64
-    dropout=0.5
-    local_updates=2
-    weight_decay=0.
-    gsl_gnn_hids=64
-    loc_gnn_outsize=64
-    glob_gnn_outsize=64
-    pretrain_out_channels=64
-    k_for_knn=0.5
-    lr=0.001
-    server_lr=0.001
-  fi
+model_type='gnn_fedgsl_gcn'
+generator='MLP-D'
+if [[ $dataset = 'cora' ]]; then
+  out_channels=7
+  op=Adam
+  hidden=256
+  dropout=0.5
+  local_updates=1
+  weight_decay=0.
+  gsl_gnn_hids=256
+  loc_gnn_outsize=256
+  glob_gnn_outsize=256
+  pretrain_out_channels=64
+  k_for_knn=15
+  lr=0.01
+elif [[ $dataset = 'citeseer' ]]; then
+  out_channels=6
+  op=Adam
+  hidden=64
+  dropout=0.5
+  local_updates=2
+  weight_decay=0.
+  gsl_gnn_hids=64
+  loc_gnn_outsize=64
+  glob_gnn_outsize=64
+  pretrain_out_channels=64
+  k_for_knn=15
+  lr=0.001
+elif [[ $dataset = 'pubmed' ]]; then
+  out_channels=3
+  op=Adam
+  hidden=64
+  dropout=0.
+  local_updates=1
+  weight_decay=0.
+  gsl_gnn_hids=256
+  loc_gnn_outsize=64
+  glob_gnn_outsize=256
+  pretrain_out_channels=256
+  k_for_knn=30
+  lr=0.03
+elif [[ $dataset = 'computers' ]]; then
+  out_channels=10
+  op=Adam
+  hidden=64
+  dropout=0.5
+  local_updates=3
+  weight_decay=0.0
+  gsl_gnn_hids=64
+  loc_gnn_outsize=256
+  glob_gnn_outsize=64
+  pretrain_out_channels=256
+  k_for_knn=20
+  lr=0.01
 fi
 
-outdir=../../../data/zhl_FedGSL_exp_out/${method}_on_${dataset}
+echo "run starts..."
 
-#请确定HPO是否为False
 for ((m = 0; m < ${#client_num[@]}; m++)); do
   for ((k = 0; k < ${#seed[@]}; k++)); do
     python federatedscope/main.py \
@@ -146,7 +76,6 @@ for ((m = 0; m < ${#client_num[@]}; m++)); do
       device ${cudaid} \
       federate.client_num ${client_num[$m]} \
       federate.sample_client_num ${client_num[$m]} \
-      dataloader.batch_size ${batch_size} \
       train.optimizer.type ${op} \
       data.type ${dataset} \
       data.splitter ${splitter} \
@@ -167,9 +96,7 @@ for ((m = 0; m < ${#client_num[@]}; m++)); do
       model.fedgsl.pretrain_out_channels $pretrain_out_channels \
       model.fedgsl.HPO False \
       early_stop.patience $patience \
-      model.fedgsl.generator $generator \
-      outdir ${outdir}/${hidden}_${dropout}_${local_updates}_${lr}_${weight_decay}_on_${dataset}_${splitter}
-    #      python  federatedscope/main.py --cfg federatedscope/FedGSL/baseline/GAT/GAT.yaml device ${cudaid} dataloader.batch_size ${batch_size} federate.client_num ${client_num[$m]}  data.type ${dataset} data.splitter ${splitter} model.out_channels ${out_channels} model.hidden ${hidden} model.dropout ${dropout} train.local_update_steps ${local_updates} train.optimizer.lr ${lr} train.optimizer.weight_decay ${weight_decay} train.optimizer.type $op seed $k outdir ${outdir}/${client_num[$m]}_clients
+      model.fedgsl.generator $generator
   done
 done
 
